b34c5c874315f6d2c8352bebef7c2eda34c0631e
Ekarry <81385639+Ekarry@users.noreply.github.com>
2021-10-16 21:05:42 +0300

    Tests Imgur_HW#4

--- src/testing/java/AccountTests.java
+++ src/testing/java/AccountTests.java
@@ -1,28 +1,25 @@
-package Testing;
 
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.proxy;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 
-public class AccountTests {
-    private Object username;
+public class AccountTests extends BaseTest {
 
     @Test
     void getAccountInfoTest() {
         given()
-                .header("Authorization", proxy)
+                .spec(requestSpecificationWithAuth)
                 .when()
-                .get("https://api.imgur.com/3/account/{username}", username)
-                .then()
-                .statusCode(200);
+                .get("https://api.imgur.com/3/account/{username}", username);
     }
+
     @Test
     void getAccountInfoWithLoggingTest() {
         given()
-                .header("Authorization","Bearer ed7328187fb5c0a24092507ce0415ab1a5f8f55c" )
+                .header("Authorization", "Bearer ed7328187fb5c0a24092507ce0415ab1a5f8f55c")
                 .log()
                 .method()
                 .log()
@@ -33,6 +30,7 @@ public class AccountTests {
                 .then()
                 .statusCode(200);
     }
+
     @Test
     void getAccountInfoWithAssertionsInGivenTest() {
         given()
@@ -49,20 +47,20 @@ public class AccountTests {
                 .contentType("application/json")
                 .when()
                 .get("https://api.imgur.com/3/account/{username}", username)
-                .prettyPeek()
+                .prettyPeek();
     }
+
     @Test
     void getAccountInfoWithAssertionsAfterTest() {
         Response response = given()
-                .header("Authorization","Bearer ed7328187fb5c0a24092507ce0415ab1a5f8f55c" )
+                .header("Authorization", "Bearer ed7328187fb5c0a24092507ce0415ab1a5f8f55c")
                 .log()
                 .method()
                 .log()
                 .uri()
                 .when()
-                .get("https://api.imgur.com/3/account/{username}", proxy.getUsername())
+                .get("https://api.imgur.com/3/account/{username}", username)
                 .prettyPeek();
-        assertThat(response.jsonPath().get("data.url"), equalTo(username));
+        assertThat(response.jsonPath(), equalTo(username));
     }
-
 }
---
+++ src/testing/java/EndPoints.java
@@ -0,0 +1,6 @@
+
+public class EndPoints {
+    public static String UPLOAD_IMAGE = "/image";
+    public final String GET_ACCOUNT = "/account/{username}";
+    public static final String FAVORITE_IMAGE = "https://api.imgur.com/3/image/{imageHash}/favorite";
+}
